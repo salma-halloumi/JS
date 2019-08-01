0398eec09ab57fc92bbce1cf716b30208aa928a6
Salma Halloumi <salmahalloumi@live.com>
2019-08-01 10:16:42 +0100

    act2 complete and debugged

--- activite_2/js/contacts.js
+++ activite_2/js/contacts.js
@@ -2,52 +2,47 @@
 Activité : gestion des contacts
 */
 
-let contacts=[];
-let option=-1;
-let input=[];
+let contacts = [];
+let input = [];
 
-class Contact{
-  constructor(nom,prenom){
-    this.nom=nom;
-    this.prenom=prenom;
+class Contact {
+  constructor(nom, prenom) {
+    this.nom = nom;
+    this.prenom = prenom;
   }
 
-  show(){
-    console.log( this.nom+" "+this.prenom+"\n" );
+  show() {
+    console.log(this.nom + " " + this.prenom + "\n");
   }
 }
 
-function insereContact(nom,prenom){
-  contacts.push(new Contact(nom,prenom))
+function insereContact(nom, prenom) {
+  contacts.push(new Contact(nom, prenom))
 }
 
-contacts[0]= new Contact("Carole", "Lévisse");
-contacts[1]= new Contact("Mélodie","Nelsonne");
+contacts[0] = new Contact("Carole", "Lévisse");
+contacts[1] = new Contact("Mélodie", "Nelsonne");
 
-function list(){
-for(let i=0;i<contacts.length;i++){
-  contacts[i].show();
-}
+function list() {
+  for (let i = 0; i < contacts.length ; i++) {
+    contacts[i].show();
+  }
 }
 
-function start(){
-while(option!==0){
-  console.log("Bienvenue dans le gestionnaire des contacts:\n 1. lister les contacts\n 2. ajouter un contact\n 0.Quitter")
-  option=window.prompt("choisir une option");
-  if(option!==1)
-  switch(option){
-    case 1:{
+function start() {
+  let option = -1;
+  while (option != 0) {
+   
+    console.log("Bienvenue dans le gestionnaire des contacts:\n 1. lister les contacts\n 2. ajouter un contact\n 0.Quitter")
+    option = window.prompt("choisir une option");
+    if (option == 1)
       list();
-      break;
+    else if (option == 2) {
+      let s = prompt("donner son nom et prenom");
+      input = s.split(" ");
+      insereContact(input[0], input[1]);
     }
-    case 2:{
-      let s=prompt("donner son nom et prenom");
-      input=s.split(" ");
-      insereContact(input[0],input[1]);
-      break;
-    }
-    case 0: break;
-    default : console.log("option invalide");
+    else if (option == 0) console.log("Au revoir");
+    else console.log("option invalide");
   }
-}
 }
